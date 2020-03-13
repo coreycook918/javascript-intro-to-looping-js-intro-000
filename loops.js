93496c591a554eca0c0e45f43a4fe7ff2739a42f
Corey Cook <cookc7438@rwps.org>
2020-03-13 16:02:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,10 @@
 let gifts = ["I am 1 strange loop.", "I am ${i} strange loops."]
 
 function forLoop(array) {
-  for (let i= 0; i < 25; i++) {
-    console.log("I am ${i} loop.")
+  for (let i = 0; i < 25; i++) {
+    if (i = 0) {
+      console.log("I am 1 strange loop.")
+  } else {
+      console.log("I am ${i} strange loops.")
   }
 }
